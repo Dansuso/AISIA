d50b9fde99919b8bd6ab8cdbc5f520b5693f2605
Daniel Suso <daniel.suso@opendeusto.es>
2024-10-29 11:11:33 +0100

    Ventana Base : Cambio en constructor
    Añadido que sea obligatorio especificar un titulo en dicho constructor, si no lanza una excepcion

--- proyectoProg3/src/gui/VentanaBase.java
+++ proyectoProg3/src/gui/VentanaBase.java
@@ -7,13 +7,28 @@ package gui;
 import javax.swing.JFrame;
 
 public class VentanaBase extends JFrame{
-
-	public VentanaBase() {
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Catalogo");
+/**
+ * Constructor de la ventana base o por defecto. 
+ * @param titulo Titulo de la ventana. Es OBLIGATORIO.
+ * @throws IllegalArgumentException si no se pasa ningun titulo como parametro
+ */
+	public VentanaBase(String titulo) {
+		
+		if(titulo == null) {
+			throw new IllegalArgumentException("Es obligatorio que la ventana tenga un titulo");
+		}
+		
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.setTitle(titulo);
 		this.setSize(640, 480);
 		this.setLocationRelativeTo(null);
 		this.setVisible(true);
+		
+		
+		
+		
+		
+		
 	}
 	
 	
