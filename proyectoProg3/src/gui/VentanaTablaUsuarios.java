0db51f08cc4b071292e130cc641561b355a1281f
ander palacin <ander.palacin@opendeusto.es>
2024-12-01 13:03:54 +0100

    Cambio ultimo psrs gusrdar todo antes de borar

--- proyectoProg3/src/gui/VentanaTablaUsuarios.java
+++ proyectoProg3/src/gui/VentanaTablaUsuarios.java
@@ -43,7 +43,7 @@ import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableRowSorter;
 
-import VentanaTabla.VentanaModificar;
+
 import domain.Usuario;
 
 public class VentanaTablaUsuarios extends JFrame {
