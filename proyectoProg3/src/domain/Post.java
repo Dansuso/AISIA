54e737722d6cd4595ea47ff972700480925a6db2
Dani Suso <danisusopz@gmail.com>
2024-12-26 12:06:46 +0100

    Clase Post : Refactorizacion
    Eliminado numLikes, numReposts, numRespuestas, cambiado LocalDate por LocalDateTime

--- proyectoProg3/src/domain/Post.java
+++ proyectoProg3/src/domain/Post.java
@@ -1,6 +1,7 @@
 package domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 /**
@@ -11,29 +12,24 @@ public class Post {
 	
 	private int codigoPost;
 	private String contenido;
-	private LocalDate fechaPost;
-	private int numLikes;
-	private int numRepost;
-	private int numRespuestas;
+	private LocalDateTime fechaPost;
 	// Referencia al CREADOR del POST
 	private Usuario creadorPost;
 	//Referencia al post que responde. Será NULL si es un post nuevo y no una respuesta a uno.
-	private Post postRespuesta;
 	
 	
-	public Post(int codigoPost, String contenido, LocalDate fechaPost, int numLikes, int numRepost, int numRespuestas,
-			Usuario creadorPost,Post postRespuesta) {
+	public Post(int codigoPost, String contenido, LocalDateTime fechaPost,Usuario creadorPost) {
 		super();
 		this.codigoPost = codigoPost;
 		this.contenido = contenido;
 		this.fechaPost = fechaPost;
-		this.numLikes = numLikes;
-		this.numRepost = numRepost;
-		this.numRespuestas = numRespuestas;
 		this.creadorPost = creadorPost;
-		this.postRespuesta = postRespuesta;
 	}
 	
+	public Usuario getCreadorPost() {
+		return creadorPost;
+	}
+
 	public int getCodigoPost() {
 		return codigoPost;
 	}
@@ -43,40 +39,13 @@ public class Post {
 	public void setContenido(String contenido) {
 		this.contenido = contenido;
 	}
-	public LocalDate getFechaPost() {
+	public LocalDateTime getFechaPost() {
 		return fechaPost;
 	}
-	public void setFechaPost(LocalDate fechaPost) {
+	public void setFechaPost(LocalDateTime fechaPost) {
 		this.fechaPost = fechaPost;
 	}
-	public int getNumLikes() {
-		return numLikes;
-	}
-	public void setNumLikes(int numLikes) {
-		this.numLikes = numLikes;
-	}
-	public int getNumRepost() {
-		return numRepost;
-	}
-	public void setNumRepost(int numRepost) {
-		this.numRepost = numRepost;
-	}
-	public int getNumRespuestas() {
-		return numRespuestas;
-	}
-	public void setNumRespuestas(int numRespuestas) {
-		this.numRespuestas = numRespuestas;
-	}
-	public Usuario getCreadorPost() {
-		return creadorPost;
-	}
-	public Post getPostRespuesta() {
-		return postRespuesta;
-	}
-
-	public void setPostRespuesta(Post postRespuesta) {
-		this.postRespuesta = postRespuesta;
-	}
+	
 
 	public void setCreadorPost(Usuario creadorPost) {
 		this.creadorPost = creadorPost;
@@ -105,9 +74,10 @@ public class Post {
 	@Override
 	public String toString() {
 		return "Post [codigoPost=" + codigoPost + ", contenido=" + contenido + ", fechaPost=" + fechaPost
-				+ ", numLikes=" + numLikes + ", numRepost=" + numRepost + ", numRespuestas=" + numRespuestas
 				+ ", creadorPost=" + creadorPost + "]";
 	}
+
+	
 	
 
 }
