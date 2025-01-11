6db3b6657ef16103cad3f7c89d723f31b9ba6cc4
Dani Suso <danisusopz@gmail.com>
2025-01-11 23:08:05 +0100

    Creado Main.

---
+++ proyectoProg3/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.SwingUtilities;
+
+import gui.VentanaInicio;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> new VentanaInicio());
+
+	}
+
+}
